29965442fc51496e084cc0d8c5b7d0facd5efcbc
Efren Lopez <edlopezm@asu.edu>
2019-07-20 13:58:48 -0700

    added big red button and listener

--- CSE535_Project/app/src/main/java/com/asu/cse535/project/HomeFragment.java
+++ CSE535_Project/app/src/main/java/com/asu/cse535/project/HomeFragment.java
@@ -4,7 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,6 +15,9 @@ import androidx.fragment.app.Fragment;
 import com.google.firebase.auth.FirebaseUser;
 
 public class HomeFragment extends Fragment {
+
+    private Button alertButton;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -21,6 +26,19 @@ public class HomeFragment extends Fragment {
         View view  = inflater.inflate(R.layout.fragment_home, container, false);
 
         TextView test = (TextView) view.findViewById(R.id.welcome);
+
+        alertButton = (Button) view.findViewById(R.id.btn);
+
+        alertButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+
+                // Replace
+                Toast.makeText( getActivity(),"Alert", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
 
         //Access mainActivity
         FirebaseUser AreYouSignInAccount = ((MainActivity) getActivity()).getAreYouSignInAccount();
